33e5699d4197e319671a0d552d784b1e448b5b99
Gabriel Roa <gaboroab14@gmail.com>
2021-03-16 00:23:55 -0400

    a bit of cleaning + game interface

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,19 +1,21 @@
 const Player = ({playerName,token,amount,action}) => {
     return(
-        <div onClick={action} className="card">
-            <header className="card-header">
-                <p className="card-header-title">
-                    {playerName}
+        <div onClick={action} className="card has-text-centered ">
+            <header className="card-header ">
+                <p className="card-header-title is-centered">
+                    <div className="has-background-warning pl-5 pr-5 pt-1 has-text-white" style={{"border-radius":"5px"}}>
+                        {playerName}
+                    </div>
                 </p>
             </header>
-            <div className="card-image">
+            <div className="card-image is-128x128">
                 <figure className="image is-4by3">
-                    <img src={token}/>
+                    <img src={token} alt="token"/>
                 </figure>
             </div>
-            <footer class="card-footer">
-                <p className="card-amount">
-                    {amount}
+            <footer className="card-footer">
+                <p className="card-footer-item has-text-black is-centered">
+                ₩ {amount}
                 </p>
             </footer>
         </div>
@@ -21,3 +23,6 @@ const Player = ({playerName,token,amount,action}) => {
 }
 
 export default Player;
+
+
+
--- src/views/Game/Index.js
+++ src/views/Game/Index.js
@@ -1,19 +1,58 @@
 import Logo from "../../components/Logo";
 import Player from "../../components/Player";
 
-const Game = () =>{
-    return(
-        <section className="section is-centered">
-            <div className="container">
-                <Logo/>
-                <Player
-                playerName="Gabriel Roa"
-                Token="http://placekitten.com/100/100"
-                amount="2.000"
-                action={()=>{}}/>
+const Game = () => {
+  return (
+    <section className="section is-centered">
+      <div className="container">
+        <Logo />
+        <div className="columns is-mobile is-centered has-text-centered mt-1">
+          <div className="column is-6">
+            <Player
+              playerName="GABOX"
+              token="http://placekitten.com/128/128"
+              amount="2.000"
+              action={() => {}}
+            />
+          </div>
+          <div className="column is-6">
+            <Player
+              playerName="ANYI"
+              token="http://placekitten.com/128/128"
+              amount="~2.000"
+              action={() => {}}
+            />
+          </div>
+        </div>
+        <div className="columns is-mobile is-half is-centered has-text-centered">
+          <div className="column is-6">
+            <Player
+              playerName="AJAV06"
+              token="http://placekitten.com/128/128"
+              amount="~2.000"
+              action={() => {}}
+            />
+          </div>
+          <div className="column is-6">
+            <Player
+              playerName="JONABB"
+              token="http://placekitten.com/128/128"
+              amount="~2.000"
+              action={() => {}}
+            />
+          </div>
+        </div>
+        <div className="columns is-mobile is-half is-centered has-text-centered">
+          <div className="column is-12">
+            <div className="box has-text-danger">
+                chao cheo
             </div>
-        </section>
-    )
-}
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
+};
 
 export default Game;
