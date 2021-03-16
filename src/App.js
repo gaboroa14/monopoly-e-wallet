a4c90d65d2c3472a67a8b412675c9ac7c99cfc27
Gabriel Roa <gaboroab14@gmail.com>
2021-03-15 21:27:21 -0400

    routing + cambios al index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import Index from './views/Index';
 import "./style/app.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <Index/>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Index/>
+        </Route>
+        <Route path="" exact>
+          
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
